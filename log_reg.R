5da199e41cd0b8a45ff0a22bf1ae857b0343ba29
Jaurell <jon.aurell@gmail.com>
2016-11-16 12:40:58 +0100

    Fixed script so it actually does log_reg
    Added family = binomial argument to glm function and -1 to exclude intercept from the model

--- log_reg.R
+++ log_reg.R
@@ -11,7 +11,7 @@ for (var in proximal_outcomes){
   vars <- sapply(c(7,9,11), function(x) sprintf('%s.%s',var,x))
   wdata[var] <- rowMeans(wdata[vars], na.rm = T)
 }
-
+prox
 for (var in c(dist2,proximal_outcomes)){
   new_var <- sprintf('%s_median',var)
   median <- median(wdata[[var]], na.rm = T)
@@ -27,14 +27,16 @@ wdata['from2016'][which(wdata['from'] == 2016),] <- 1
 out <- c()
 for (var in dist2){
   var <- sprintf('%s_median',var)
-  s <- glm(get(var) ~ nsfs_c_mean_median + nsfs_r_mean_median + roleclarity_mean_median + from2016, data = wdata)
-  ss <- summary(lm.beta(s))
+  s <- glm(get(var) ~ nsfs_c_mean_median + nsfs_r_mean_median + roleclarity_mean_median + from2016 -1 , data = wdata, family = 'binomial') # both split
+  #s <- glm(get(var) ~ nsfs_c_mean + nsfs_r_mean + roleclarity_mean + from2016 -1 , data = wdata, family = 'binomial') # only dependet split
+  #ss <- summary(lm.beta(s))
+  ss <- summary(s)
   n <- nobs(s)
-  fstat <- ss$fstatistic
-  f <- round(fstat[['value']],2)
-  p <- pf(fstat[1],fstat[2],fstat[3],lower.tail=F)
-  r2 <- round(ss$r.squared,2)
-  cs <- coef(ss)[2:5,2:5]
+  #fstat <- ss$fstatistic
+  #f <- round(fstat[['value']],2)
+  #p <- pf(fstat[1],fstat[2],fstat[3],lower.tail=F)
+  #r2 <- round(ss$r.squared,2)
+  cs <- coef(ss)
   c <- t(sapply(1:4, function(x) as.character(round(c(cs[x,1],cs[x,2],cs[x,3]),2))))
   for (x in 1:4 ){
     px <- ''
@@ -50,13 +52,12 @@ for (var in dist2){
     c[x,1] <- sprintf('%s%s',c[x,1],px)
   }
 
-  OR <- exp(cbind(OR = coef(s), confint(s)))
+  OR <- round(exp(cbind(OR = coef(s), confint(s))),2)
   preds <- c(c[1,],OR[1,],c[2,],OR[2,],c[3,],OR[3,],c[4,],OR[4,])
-  out_temp <- c(var,n,f,p,r2,preds)
+  out_temp <- c(var,n,preds)
   #cbind(n,f,p,r2,c[1,],c[2,],c[3,],c[4,])
   #out_temp <- cbind(n,f,round(p,4),r2,preds)
   out <- rbind(out,out_temp)
 }
-colnames(out) <- c('Dependent','N','F','P','R2','nsfs_c_median_beta','SE','t','OR','lower','upper','nsfs_r_median_beta','SE','t','OR','lower','upper','roleclarity_median_beta','SE','t','OR','lower','upper','from2016_beta','SE','t','OR','lower','upper')
+colnames(out) <- c('Dependent','N','nsfs_c_median_beta','SE','z','OR','lower','upper','nsfs_r_median_beta','SE','z','OR','lower','upper','roleclarity_median_beta','SE','z','OR','lower','upper','from2016_beta','SE','z','OR','lower','upper')
 write.csv(out,'reg.csv',row.names = F)
-
